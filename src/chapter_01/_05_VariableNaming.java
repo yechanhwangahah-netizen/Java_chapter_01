be06bfd518187169bcdeb3887ac03e341c1094af
yechanhwangahah <yechanhwangahah@gmail.com>
2026-03-19 15:24:51 +0900

    05_ Variable

---
+++ src/chapter_01/_05_VariableNaming.java
@@ -0,0 +1,33 @@
+package chapter_01;
+
+public class _05_VariableNaming {
+    public static void main(String[] args) {
+        //변수 이름 짓는 법
+        //1. 저장할 값에 어울리는 이름
+//        2. 밑줄(_) 문자(abc), 숫자 가능
+        //3. 한 단어 또는 2개 이상의 단어의 연속
+
+
+        //입국 신고서(여행)
+
+        String nationailty = "대한민국";
+        String firstName = "ㅇㅖ찬";
+        String lastName = "황";
+        String dateBirth = "2006-10-04";
+
+        String fligtNo = "kE393";
+        String _flightNo = "kE393";
+
+        int acocompany =2;
+        int lethStay =5;
+
+        String item1 = "ㅅㅣ계";
+        String item2 = "책상";
+
+        int i =0;
+
+        //절대 변하지 않는 상수는 대문자로
+        final String CODE = "KR";
+
+    }
+}
