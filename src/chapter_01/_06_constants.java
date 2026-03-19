d9d78de4cd3ea21b2502622526f4fd02cb3e24d9
yechanhwangahah <yechanhwangahah@gmail.com>
2026-03-19 15:28:03 +0900

    constants

---
+++ src/chapter_01/_06_constants.java
@@ -0,0 +1,16 @@
+package chapter_01;
+
+public class _06_constants {
+
+    static void main(){
+        String KR_CONTRY_CODE= "+82";
+        KR_CONTRY_CODE = "8282";
+        System.out.println(KR_CONTRY_CODE);
+
+        final double PI = 3.141592;
+        final String DATE_OF_BIRTH= "2006-10-04";
+
+    }
+    }
+
+
